db3a8a53cfcc19c0ecfdc008171e961154829898
haojunjie <1275119618@qq.com>
2023-01-08 13:31:49 +0800

    feat: 提交版本

---
+++ src/main/java/com/hjj/dazzling/DazzlingApplication.java
@@ -0,0 +1,13 @@
+package com.hjj.dazzling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DazzlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DazzlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hjj/dazzling/DazzlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjj.dazzling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DazzlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
